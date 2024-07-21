b936fddf665ee650e60f167995d794918bf3f5e2
ARDRASATHYAN <sardra9988@gmail.com>
2024-07-21 15:18:04 +0530

    login & signup

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const cors = require('cors');
+const router = require('./backend/src/routers/userRouter');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+//routes
+app.use('/user',router)
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
